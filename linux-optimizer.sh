a0062961703f0a4e225d208c7ca699dac96d0128
ﾑ丂んズﾑ刀 <108182589+devilofcyber@users.noreply.github.com>
2026-01-28 22:20:38 +0330

    Update linux-optimizer.sh

--- linux-optimizer.sh
+++ linux-optimizer.sh
@@ -111,7 +111,7 @@ fix_dns(){
     sed -i '/nameserver/d' $DNS_PATH
 
     echo "nameserver 1.1.1.1" >> $DNS_PATH
-    echo "nameserver 1.0.0.1" >> $DNS_PATH
+    echo "nameserver 8.8.8.8" >> $DNS_PATH
     
     green_msg "DNS Fixed Temporarily."
     echo 
